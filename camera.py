12b130f51570439f0675dd2c6eeaf7d4692f10b6
Ariel Houri <109424122+arielhouri@users.noreply.github.com>
2025-05-04 16:04:50 +0300

    Update camera.py

--- camera.py
+++ camera.py
@@ -9,8 +9,8 @@ camera_index = 0
 cap = cv2.VideoCapture(camera_index)
 started = False
 # Optional: Set camera exposure
-cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
-cap.set(cv2.CAP_PROP_EXPOSURE, -6)
+#cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25)
+#cap.set(cv2.CAP_PROP_EXPOSURE, -6)
 
 print("The binary is: ", end="")
 if not cap.isOpened():
@@ -27,7 +27,7 @@ while True:
     a = time.time()
     time.sleep(0.25)  # Wait 1 second before capturing again
     amount = 0
-    for i in range(3):
+    for i in range(5):
         # Capture a frame
         ret, frame = cap.read()
 
@@ -41,11 +41,11 @@ while True:
         # Loop through each pixel
         frame = frame.astype(np.int16)
         b,g,r = cv2.split(frame)
-        led_mask = ((g - b) > 50) & ((g - r) > 100) & (g > 110)
+        led_mask = ((g - b) > 50) & ((g - r) > 80) & (g > 90)
         led_found = np.any(led_mask)
         amount += 1 if led_found else 0
     # Print result every second
-    c = "1" if (amount>=2) else "0"
+    c = "1" if (amount>=3) else "0"
     binary += c
     if c == "1" and not started:
         started = True
@@ -54,23 +54,24 @@ while True:
     # Check if last 16 characters are all '0'
     if binary[-16:] == '0' * 16:
         binary = binary[:-16]  # Remove the last 16 characters
-        break
+        started = False
+        # Pad with leading zeros until length is divisible by 8
+        while len(binary) % 8 != 0:
+            binary = binary + '0'
+
+        ascii_text = ''.join(
+            chr(int(binary[i:i + 8], 2))
+            for i in range(0, len(binary), 8)
+        )
+
+        print("")
+        print(ascii_text)
+        binary = ""
+        continue
     b = time.time()
     time.sleep(1-(b-a))
 
-# Pad with leading zeros until length is divisible by 8
-while len(binary) % 8 != 0:
-    binary = binary + '0'
 
-
-ascii_text = ''.join(
-    chr(int(binary[i:i+8], 2))
-    for i in range(0, len(binary), 8)
-)
-
-
-print("")
-print(ascii_text)
 
 # Cleanup
 cap.release()
